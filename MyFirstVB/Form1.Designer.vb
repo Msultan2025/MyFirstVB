dcb964386259328267a4c3681e3d6a3afc6645a9
Msultan2025 <moh776711@gmail.com>
2025-09-22 23:58:16 +0300

    Initial commit

---
+++ MyFirstVB/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        Label1.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(152, 49)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(249, 23)
+        Label1.TabIndex = 0
+        Label1.Text = "الاسم: محمد سعيد محمد السلطان"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        Label2.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(119, 108)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(282, 23)
+        Label2.TabIndex = 1
+        Label2.Text = "Information Technology :التخصص "
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        Label3.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(279, 169)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(122, 23)
+        Label3.TabIndex = 2
+        Label3.Text = "المستوى: الثاتي"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.BlanchedAlmond
+        ClientSize = New Size(524, 368)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "مشروعي الاول"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+
+End Class
